4584dfbc8128babed74f4e432de43ad99cdaa7ff
Nigel Dao <ndao1@mgb.org>
2025-05-16 22:50:20 -0400

    initial push

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use rand::seq::IndexedRandom;
+
+fn main() {
+    
+    println!("Enter the choices (comma separated):");
+    let mut choices = String::new();
+    io::stdin()
+        .read_line(&mut choices)
+        .expect("Failed to read choices.");
+
+    let choices: Vec<&str> = choices.split(",").collect();
+    match choices.choose(&mut rand::rng()) {
+        Some(i) => println!("{}", i.trim()),
+        None    => println!("Nothing!")
+    }
+}
